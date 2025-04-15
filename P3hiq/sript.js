f39af81c09b1245d3365d1c86cfc626477920260
hiqmal7 <hiqiimall@gamil.com>
2025-04-15 09:41:05 +0700

    Menambahkan index.html

---
+++ P3hiq/sript.js
@@ -0,0 +1,36 @@
+// Document.writeln("Hello Word")
+
+// var nama ="jhone"
+// let umur = 9
+// umur= 10
+
+// count kelas = "A"
+// let isActive= true
+
+// Document.writeln(umur)
+// Document.writeln(kelas)
+
+// doDocument.writeln(typeof(nama))
+// prompt('MAsukan nama')
+
+// let nama = prompt('nama')
+
+
+
+
+// let arr = ["apel," "Mangga",90, true,null]
+
+// for (let X = 0, x < array.length; x++){
+//     document.writeln(array[X])
+// }
+//  array.push(100)
+let perdon = {
+    nama:'jhone'
+    umur:24,
+    kelas: "A"
+
+}
+perdon.nama= "kevin"
+document.writeln(perdon.nama)
+document.writeln(perdon.umur)
+document.writeln(perdon.kelas)
